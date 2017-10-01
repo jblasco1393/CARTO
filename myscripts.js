b34ced2f5569e6ab4ee9b06a08d2ea35de134fb9
Javier Blasco <32306576+jblasco1393@users.noreply.github.com>
2017-10-01 17:19:47 +0200

    0

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,4 @@
+//Side Navigation Bar (modal)
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -8,29 +9,21 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
 }
 
-// Set the date we're counting down to
+//CountDown Blog
 var countDownDate = new Date("Nov 1, 2017 11:59:00").getTime();
 
-// Update the count down every 1 second
 var countdownfunction = setInterval(function() {
-
-    // Get todays date and time
     var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
     var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="demo"
     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
     
-    // If the count down is over, write some text 
+    // When countdown is over, display the messege. 
     if (distance < 0) {
         clearInterval(countdownfunction);
         document.getElementById("demo").innerHTML = "The time has come, ENJOY";
